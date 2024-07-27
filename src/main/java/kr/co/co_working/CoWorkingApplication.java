b19f2bff0bee10f41ded882a8842cc9191e5d4a1
brobro332 <332bro@naver.com>
2024-07-27 16:03:35 +0900

    프로젝트 : 프로젝트 생성 및 설정

---
+++ src/main/java/kr/co/co_working/CoWorkingApplication.java
@@ -0,0 +1,13 @@
+package kr.co.co_working;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoWorkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoWorkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/co_working/CoWorkingApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.co_working;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoWorkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
