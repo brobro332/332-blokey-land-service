7dcf396743a4dd0418637753d7047ad74ecf4995
brobro332 <332bro@naver.com>
2024-08-10 10:51:16 +0900

    테스트 : Project, Task D 테스트 코드 개발

--- src/test/java/kr/co/co_working/task/service/TaskServiceTest.java
+++ src/test/java/kr/co/co_working/task/service/TaskServiceTest.java
@@ -14,11 +14,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-//@Transactional
+@Transactional
 class TaskServiceTest {
     @Autowired
     TaskRepository taskRepository;
@@ -30,7 +31,6 @@ class TaskServiceTest {
     ProjectService projectService;
 
     @Test
-    @Transactional
     public void createTask() throws Exception {
         /* given */
         ProjectRequestDto.CREATE projectDto = new ProjectRequestDto.CREATE();
@@ -38,6 +38,7 @@ class TaskServiceTest {
         projectDto.setDescription("프로젝트 관리 프로그램 만들기");
         projectDto.setTasks(new ArrayList<>());
         Long projectId = projectService.createProject(projectDto);
+        projectRepository.flush();
 
         TaskRequestDto.CREATE taskDto = new TaskRequestDto.CREATE();
         taskDto.setName("테스트테스트테스트테스트테스트테스트테스");
@@ -47,12 +48,11 @@ class TaskServiceTest {
 
         /* when */
         Long taskId = taskService.createTask(taskDto);
+        taskRepository.flush();
 
         /* then */
         Project project = projectRepository.findById(projectId).get();
-        projectRepository.flush();
         Task task = taskRepository.findById(taskId).get();
-        taskRepository.flush();
 
         Assertions.assertEquals(projectDto.getName(), project.getName());
         Assertions.assertEquals(projectDto.getDescription(), project.getDescription());
@@ -70,10 +70,49 @@ class TaskServiceTest {
     }
 
     @Test
-    void updateTask() {
+    public void updateTask() throws Exception {
+        /* given */
+
+        /* when */
+
+        /* then */
     }
 
     @Test
-    void deleteTask() {
+    public void deleteTask() throws Exception {
+        /* given */
+        ProjectRequestDto.CREATE projectDto = new ProjectRequestDto.CREATE();
+        projectDto.setName("프로젝트 A");
+        projectDto.setDescription("프로젝트 관리 프로그램 만들기");
+        projectDto.setTasks(new ArrayList<>());
+        Long projectId = projectService.createProject(projectDto);
+
+        List<Long> taskIdList = new ArrayList<>();
+        for (int i = 1; i <= 2; i++) {
+            TaskRequestDto.CREATE taskDto = new TaskRequestDto.CREATE();
+            taskDto.setName("테스트 " + i);
+            taskDto.setType("텍스트 " + i);
+            taskDto.setDescription("금주 프로젝트 개발 건에 대한 테스트 " + i);
+            taskDto.setProjectId(projectId);
+
+            taskIdList.add(taskService.createTask(taskDto));
+            taskDto = null;
+        }
+
+        TaskRequestDto.DELETE taskDeleteDto = new TaskRequestDto.DELETE();
+        taskDeleteDto.setProjectId(projectId);
+
+        /* when */
+        taskService.deleteTask(taskIdList.get(0), taskDeleteDto);
+
+        /* then */
+        Project project = projectRepository.findById(projectId).get();
+        Task task = taskRepository.findById(taskIdList.get(1)).get();
+
+        Assertions.assertEquals("테스트 2", task.getName());
+        Assertions.assertEquals("텍스트 2", task.getType());
+        Assertions.assertEquals("금주 프로젝트 개발 건에 대한 테스트 2", task.getDescription());
+
+        Assertions.assertEquals(1, project.getTasks().size());
     }
 }
