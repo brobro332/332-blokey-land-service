789a6f7f37047b384e2aa7e43992768736b8a388
brobro332 <332bro@naver.com>
2024-08-13 22:19:25 +0900

    기능추가 : Team CRUD API 개발

---
+++ src/main/java/kr/co/co_working/team/controller/TeamApiController.java
@@ -0,0 +1,65 @@
+package kr.co.co_working.team.controller;
+
+import kr.co.co_working.common.dto.ResponseDto;
+import kr.co.co_working.team.dto.TeamRequestDto;
+import kr.co.co_working.team.service.TeamService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class TeamApiController {
+    private final TeamService service;
+
+    /**
+     * createTeam : Team 등록
+     * @param dto
+     * @return
+     * @throws Exception
+     */
+    @PostMapping("/api/v1/team")
+    public ResponseDto<?> createTeam(@RequestBody TeamRequestDto.CREATE dto) throws Exception {
+        service.createTeam(dto);
+
+        return ResponseDto.ofSuccess("팀 등록에 성공했습니다.");
+    }
+
+    /**
+     * readTeam : Team 조회
+     * @param dto
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/api/v1/team")
+    public ResponseDto<?> readTeam(@RequestBody TeamRequestDto.READ dto) throws Exception {
+        return ResponseDto.ofSuccess("팀 조회에 성공했습니다.", service.readTeam(dto));
+    }
+
+    /**
+     * updateTeam : Team 수정
+     * @param id
+     * @param dto
+     * @return
+     * @throws Exception
+     */
+    @PutMapping("/api/v1/team/{team_id}")
+    public ResponseDto<?> updateTeam(@PathVariable(name = "team_id") Long id,
+                                     @RequestBody TeamRequestDto.UPDATE dto) throws Exception {
+        service.updateTeam(id, dto);
+
+        return ResponseDto.ofSuccess("팀 수정에 성공했습니다.");
+    }
+
+    /**
+     * 
+     * @param id
+     * @return deleteTeam : Team 삭제
+     * @throws Exception
+     */
+    @DeleteMapping("/api/v1/team/{team_id}")
+    public ResponseDto<?> deleteTeam(@PathVariable(name = "team_id") Long id) throws Exception {
+        service.deleteTeam(id);
+
+        return ResponseDto.ofSuccess("팀 삭제에 성공했습니다.");
+    }
+}
---
+++ src/main/java/kr/co/co_working/team/service/TeamService.java
@@ -0,0 +1,92 @@
+package kr.co.co_working.team.service;
+
+import kr.co.co_working.team.dto.TeamRequestDto;
+import kr.co.co_working.team.dto.TeamResponseDto;
+import kr.co.co_working.team.repository.TeamDslRepository;
+import kr.co.co_working.team.repository.TeamRepository;
+import kr.co.co_working.team.repository.entity.Team;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class TeamService {
+    private final TeamRepository repository;
+    private final TeamDslRepository dslRepository;
+
+    /**
+     * createTeam : Team 등록
+     * @param dto
+     * @return
+     * @throws Exception
+     */
+    public Long createTeam(TeamRequestDto.CREATE dto) throws Exception {
+        // 1. Team 빌드
+        Team team = Team.builder()
+                .name(dto.getName())
+                .description(dto.getDescription())
+                .build();
+
+        // 2. Team 등록
+        repository.save(team);
+
+        // 3. ID 반환
+        return team.getId();
+    }
+
+    /**
+     * readTeam : Team 조회
+     * @param dto
+     * @return
+     * @throws Exception
+     */
+    public List<TeamResponseDto> readTeam(TeamRequestDto.READ dto) throws Exception {
+        // QueryDSL 동적 쿼리 결과 반환
+        return dslRepository.readTeamList(dto);
+    }
+
+    /**
+     * updateTeam : Team 수정
+     * @param dto
+     * @return
+     * @throws Exception
+     */
+    @Transactional
+    public void updateTeam(Long id, TeamRequestDto.UPDATE dto) throws Exception {
+        // 1. Team 조회
+        Optional<Team> selectedTeam = repository.findById(id);
+
+        // 2. 부재 시 예외 처리
+        if (selectedTeam.isEmpty()) {
+            throw new NoSuchElementException("수정하려는 팀이 존재하지 않습니다. ID : " + id);
+        }
+
+        // 3. 존재 시 수정 처리
+        Team team = selectedTeam.get();
+        team.updateTeam(dto.getName(), dto.getDescription());
+    }
+
+    /**
+     * deleteTeam : Team 삭제
+     * @param id
+     * @throws Exception
+     */
+    @Transactional
+    public void deleteTeam(Long id) throws Exception {
+        // 1. Team 조회
+        Optional<Team> selectedTeam = repository.findById(id);
+
+        // 2. 부재 시 예외 처리
+        if (selectedTeam.isEmpty()) {
+            throw new NoSuchElementException("삭제하려는 팀이 존재하지 않습니다. ID : " + id);
+        }
+        
+        // 3. 존재 시 삭제 처리
+        repository.delete(selectedTeam.get());
+    }
+}
