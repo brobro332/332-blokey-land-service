5b34a981154e7c2c1e3ca18d58801e9d64647a26
ᴊɪɴ-ʜʏᴇᴏɴɢ ᴋɪᴍ <332bro@naver.com>
2025-01-12 20:18:40 +0900

    CoWorkingApplicationTests.java 업데이트

--- spring-app/src/test/java/kr/co/co_working/CoWorkingApplicationTests.java
+++ spring-app/src/test/java/kr/co/co_working/CoWorkingApplicationTests.java
@@ -2,8 +2,10 @@ package kr.co.co_working;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CoWorkingApplicationTests {
 
 	@Test
