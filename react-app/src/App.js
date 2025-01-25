648acd7e5585d095ce7a00b3a4272a5dc56cdf04
brobro332 <332bro@naver.com>
2025-01-25 17:03:31 +0900

    프로젝트 : 리액트 애플리케이션 기존 소스파일 제거 및 메인화면 레이아웃 개발

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,28 +1,63 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginForm from "./components/forms/LoginForm";
-import PolicyForm from "./components/forms/PolicyForm";
-import JoinForm from "./components/forms/JoinForm";
-import JoinCompleteForm from "./components/forms/JoinCompleteForm";
+import React from 'react';
+import { Container, AppBar, Toolbar, Typography, Box, Drawer, List, ListItem, ListItemText, CssBaseline } from '@mui/material';
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Routes>
-        {/* 로그인 페이지 */}
-        <Route path="/" element={<LoginForm />} />
-        
-        {/* 회원가입 약관동의 페이지 */}
-        <Route path="/policy-form" element={<PolicyForm />} />
+    <Box sx={{ display: 'flex' }}>
+      {/* 기본 스타일 초기화 */}
+      <CssBaseline />
 
-        {/* 회원가입 개인정보입력 페이지 */}
-        <Route path="/join-form" element={<JoinForm />} />
+      {/* 고정 헤더 */}
+      <AppBar position="fixed" sx={{ width: '100%', zIndex: 1201 }}>
+        <Toolbar>
+          <Typography variant="h6">삼사미 프로젝트 매니저</Typography>
+        </Toolbar>
+      </AppBar>
 
-        {/* 회원가입 완료 페이지 */}
-        <Route path="/join-complete-form" element={<JoinCompleteForm />} />
-      </Routes>
-    </Router>
+      {/* 사이드바 */}
+      <Drawer
+        sx={{
+          width: 240,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: 240,
+            boxSizing: 'border-box',
+            top: 64,  // AppBar 높이만큼 여백 추가
+            height: 'calc(100% - 64px)',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <List sx={{ size: 'lg' }}>
+          <ListItem button>
+            <ListItemText primary="타임라인" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary="프로젝트 관리" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary="스프린트" />
+          </ListItem>
+        </List>
+      </Drawer>
+
+      {/* 메인 콘텐츠 영역 */}
+      <Box
+        component="main"
+        sx={{
+          bgcolor: 'background.default',
+          pt: 10
+        }}
+      >
+        {/* 메인 컨텐츠 */}
+        <Container sx={{ maxWidth: 'lg' }}>
+          <Typography variant="h4">메인 콘텐츠</Typography>
+          <Typography variant="body1">여기에 콘텐츠를 추가할 수 있습니다.</Typography>
+        </Container>
+      </Box>
+    </Box>
   );
-}
+};
 
 export default App;
