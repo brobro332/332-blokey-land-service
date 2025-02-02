2a4cf737e460b5170475d647e6cdf699b6350b8c
brobro332 <332bro@naver.com>
2025-02-02 19:15:58 +0900

    리팩토링 : Invitation API 엔드포인트 추가

--- spring-app/src/main/java/kr/co/co_working/invitation/controller/InvitationApiController.java
+++ spring-app/src/main/java/kr/co/co_working/invitation/controller/InvitationApiController.java
@@ -19,37 +19,38 @@ public class InvitationApiController {
     private final InvitationService service;
 
     /**
-     * createInvitation : 워크스페이스 가입요청 생성
+     * createInvitation : Invitation 생성
      * @param dto
      * @return
      * @throws NoSuchElementException
      * @throws Exception
      */
-    @PostMapping
+    @PostMapping("/api/v1/invitation")
     public ResponseDto<?> createInvitation(InvitationRequestDto.CREATE dto) throws NoSuchElementException, Exception {
         service.createInvitation(dto);
         return ResponseDto.ofSuccess("워크스페이스 가입요청 등록에 성공했습니다.");
     }
 
     /**
-     * readInvitation : 워크스페이스 가입요청 조회
+     * readInvitation : Invitation 조회
      * @param dto
      * @return
      * @throws NoSuchElementException
      * @throws Exception
      */
+    @GetMapping("/api/v1/invitation")
     public ResponseDto<?> readInvitation(InvitationRequestDto.READ dto) throws NoSuchElementException, Exception {
         return ResponseDto.ofSuccess("워크스페이스 가입요청 조회에 성공했습니다.", service.readInvitation(dto));
     }
 
     /**
-     * deleteInvitation : 워크스페이스 가입요청 삭제
+     * deleteInvitation : Invitation 삭제
      * @param dto
      * @return
      * @throws NoSuchElementException
      * @throws Exception
      */
-    @DeleteMapping
+    @DeleteMapping("/api/v1/invitation")
     public ResponseDto<?> deleteInvitation(InvitationRequestDto.DELETE dto) throws NoSuchElementException, Exception  {
         service.deleteInvitation(dto);
         return ResponseDto.ofSuccess("워크스페이스 가입요청 삭제에 성공했습니다.");
