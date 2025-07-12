f8e9685f57425876a5de7c247f0d6b8cd93d4e63
brobro332 <332bro@naver.com>
2025-07-12 11:54:04 +0900

    테스트 : TestContainers 의존성 추가

--- spring-boot-app/src/test/java/xyz/samsami/blokey_land/BlokeyLandApplicationTests.java
+++ spring-boot-app/src/test/java/xyz/samsami/blokey_land/BlokeyLandApplicationTests.java
@@ -4,9 +4,26 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class BlokeyLandApplicationTests {
+	@Container
+	static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>(
+		"postgres:15-alpine");
+
+	@DynamicPropertySource
+	static void overrideProps(org.springframework.test.context.DynamicPropertyRegistry registry) {
+		registry.add("DATA_POSTGRES_URI", postgresContainer::getJdbcUrl);
+		registry.add("DATA_POSTGRES_USERNAME", postgresContainer::getUsername);
+		registry.add("DATA_POSTGRES_PASSWORD", postgresContainer::getPassword);
+		registry.add("FILE_UPLOAD_DIR", () -> "/test-uploads/");
+	}
+
 	@BeforeAll
 	static void init() {
 		Dotenv dotenv = Dotenv.load();
